e81dbbfdbcc1cb23ff795effd6d5ab0bcb4de7b9
RyoUchida0415 <tkkndshr2565@gmail.com>
2020-10-28 19:39:01 +0900

    add jpostal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,20 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery
+//= require jquery.jposta
 //= require_tree .
+
+$(function() {
+  $(document).on('turbolinks:load', () => {
+    $('#dam_postal_code').jpostal({
+      postcode : [
+        '#dam_postal_code'
+      ],
+      address: {
+        "#dam_prefecture_code": "%3", // # 都道府県が入力される
+        "#dam_city"           : "%4%5", // # 市区町村と町域が入力される
+        "#dam_street"         : "%6%7" // # 大口事務所の番地と名称が入力される
+      }
+    });
+  });
+});
--- app/models/dam.rb
+++ app/models/dam.rb
@@ -17,6 +17,8 @@ class Dam < ApplicationRecord
     has_many :post_comments, dependent: :destroy
     has_many :favorites, dependent: :destroy
 
+    validates :title, presence: true
+
     def favorited_by?(user)
       favorites.where(user_id: user.id).exists?
     end
