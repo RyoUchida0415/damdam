4dd0d42e16c0d17b558b2b1d9347ba8ed870d5c0
RyoUchida0415 <tkkndshr2565@gmail.com>
2020-11-02 19:37:26 +0900

    add favorite show link

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,6 @@ class FavoritesController < ApplicationController
     def index
         @user = current_user
         @favorites = Favorite.where(user_id: @user.id)
-        #dam = Dam.find(params[:id])
     end
 
 	def create
