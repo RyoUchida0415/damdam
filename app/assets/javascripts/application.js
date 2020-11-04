360dee136c2ae18036225f56789206b813e052a8
RyoUchida0415 <tkkndshr2565@gmail.com>
2020-11-04 21:13:50 +0900

    add Hamburger Menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,3 +31,12 @@ $(function() {
     });
   });
 });
+
+//ヘッダーのハンバーガーメニュー
+$(document).on('turbolinks:load', function() {
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
+    event.preventDefault();
+  });
+});
