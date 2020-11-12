04e457bf419454f8e1e7b7f1dcd60e354d4594dc
RyoUchida0415 <tkkndshr2565@gmail.com>
2020-11-12 14:54:17 +0900

    can preview

--- app/assets/javascripts/image_preview.js
+++ app/assets/javascripts/image_preview.js
@@ -1,12 +1,14 @@
 $(function () {
   function readURL(input) {
-    if (input.files && input.files[0]) {
+      //画像を読み込む
       var reader = new FileReader();
+      //画像URLをimgに追加
+      //画像ファイルが読み込んだら、処理が実行される
       reader.onload = function (e) {
         $('.img_prev').attr('src', e.target.result);
       }
+      //画像ファイルのURLを取得
       reader.readAsDataURL(input.files[0]);
-    }
   }
 
   $('.img_field').change(function () {
